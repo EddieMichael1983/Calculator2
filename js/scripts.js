7017931e21baa5e2725706f21ed7b71761df312e
Eddie Michael <eddiemichael1983@gmail.com>
2020-09-23 15:19:54 -0700

    add initial HTML and JS files

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function add(number1, number2) {
+	return number1+ number2;
+}
+
+const number1 = parseInt(prompt("Enter price number 1:"));
+const number2 = parseInt(prompt("Enter price number 2:"));
+
+alert(add(number1, number2));
